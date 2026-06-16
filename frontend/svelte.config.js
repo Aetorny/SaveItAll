3c1b3492393e21f028cdc0975629795c12ce7c14
Aetorny <aetorny@gmail.com>
2026-06-16 05:50:55 +0300

    update for deploy

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDeploy = process.env.DEPLOY === 'true';
+
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
@@ -8,6 +10,9 @@ export default {
 			pages: 'dist',
 			assets: 'dist',
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: isDeploy ? '/SaveItAll' : ''
+		}
 	}
 };
